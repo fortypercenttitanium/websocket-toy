52b908002957d4a726c113a94c19f27b1e083f70
Alex Younger <alexanderyounger@gmail.com>
2020-12-16 09:48:13 -0500

    merged branches

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
+const socket = new WebSocket('ws://localhost:8081');
+socket.onopen = function (event) {
+	console.log('connected!');
+};
+socket.onerror = function (error) {
+	console.error(error);
+};
+
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-	const socket = new WebSocket('ws://localhost:8081');
-	socket.onopen = function (event) {
-		console.log('connected!');
-	};
+	document.querySelector('.connection-status').textContent = socket.isAlive
+		? ' connected.'
+		: ' not connected.';
 });
